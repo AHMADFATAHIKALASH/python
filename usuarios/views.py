2020944963f5078e2f57f9c1680fa1b86d629ecd
AHMAD KALASH <114625236+AHMADFATAHIKALASH@users.noreply.github.com>
2023-04-12 00:30:50 -0300

    .

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        print(username, email, senha, confirmar_senha)
+        return HttpResponse('Test....')
+    
