d831d7dbefae8cb75df1fc29c9aadc713f2907e2
AHMAD KALASH <114625236+AHMADFATAHIKALASH@users.noreply.github.com>
2023-04-16 17:18:44 -0300

    .

--- eventos/models.py
+++ eventos/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Evento(models.Model):
     criador = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, blank=True)
     nome = models.CharField(max_length=200)
@@ -19,6 +20,13 @@ class Evento(models.Model):
     cor_fundo = models.CharField(max_length=7)
 
     def __str__(self):
-        return self.nome
+        return self.nome 
+    
+
+class Certificado(models.Model):
+    certificado = models.ImageField(upload_to="certificados")
+    participante = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    evento = models.ForeignKey(Evento, on_delete=models.DO_NOTHING)
+
     
 
